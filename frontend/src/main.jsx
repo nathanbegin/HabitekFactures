7c1ce53d3a0df1a8a330ba00604540ec76eb624c
Nathan Begin <nathanbegin@hotmail.com>
2025-05-04 21:38:01 -0400

    config

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'      // ← impératif !
+import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><App /></React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root'))
+         .render(<React.StrictMode><App/></React.StrictMode>)
