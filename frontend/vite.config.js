0a5dd5cd497879a672e0e8b533293d339526db4f
Nathan Begin <nathanbegin@hotmail.com>
2025-05-04 20:55:25 -0400

    Update majeure

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
+  css: {
+    postcss: './postcss.config.cjs'
+  },
+  server: {
+    open: true,
+    port: 5173,
+    hmr: {
+      // passe à false pour désactiver l'overlay d'erreur si besoin
+      overlay: true
+    }
+  }
+});
